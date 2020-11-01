8f6e6f2add5435d4a5d51c59c44c50a9f68fe46d
dipesh <kcdipesh429@gmail.com>
2020-11-01 23:52:56 +0545

    feat:routes setup done

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"go.uber.org/fx"
+)
+
+// Module exports dependency to container
+var Module = fx.Options(
+	fx.Provide(NewUserRoutes),
+	fx.Provide(NewRoutes),
+)
+
+// Routes contains multiple routes
+type Routes []Route
+
+// Route interface
+type Route interface {
+	Setup()
+}
+
+// NewRoutes sets up routes
+func NewRoutes(user UserRoutes) Routes {
+	return Routes{
+		user,
+	}
+}
+
+// Setup all the route
+func (r Routes) Setup() {
+	for _, route := range r {
+		route.Setup()
+	}
+}
--- api/routes/user.go
+++ api/routes/user.go
@@ -5,12 +5,33 @@ import (
 	"golang-fx/infrastructure"
 )
 
-//UserRoutes -> setting routes for user entity
-func UserRoutes(routes infrastructure.GinRouter, logger infrastructure.Logger, controller controllers.UserController) {
-	logger.Zap.Info("Setting up user routes")
+// UserRoutes model
+type UserRoutes struct {
+	Logger     infrastructure.Logger
+	Handler    infrastructure.GinRouter
+	Controller controllers.UserController
+}
 
-	userGroup := routes.Gin.Group("/clients-assigned-to")
-	{
-		userGroup.GET("/", controller.GetAllUser)
+// NewUserRoutes creates a new health check routes
+func NewUserRoutes(
+	logger infrastructure.Logger,
+	handler infrastructure.GinRouter,
+	controller controllers.UserController,
+) UserRoutes {
+	return UserRoutes{
+		Logger:     logger,
+		Handler:    handler,
+		Controller: controller,
 	}
+}
+
+// Setup sets up routes
+func (s UserRoutes) Setup() {
+	s.Logger.Zap.Info("Setting up user routes")
+	client := s.Handler.Gin.Group("/users")
+	{
+		client.GET("/", s.Controller.GetAllUser)
+
+	}
+
 }
